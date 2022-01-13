7e93ba55ff8615fe907e88a473347fa1226e38cd
Sander <s.b.koomen@student.utwente.nl>
2022-01-13 14:01:43 +0100

    fix saving to work with current folder structure

--- instrument_classifier/train/save.py
+++ instrument_classifier/train/save.py
@@ -4,4 +4,4 @@ from instrument_classifier.utils.paths import model_path
 
 
 def save_model(nn, model_name):
-    torch.save(nn.state_dict(), os.path.join(model_path, model_name, model_name + 'tar'), _use_new_zipfile_serialization=False)
+    torch.save(nn.state_dict(), os.path.join(model_path, 'self-trained/', model_name + 'tar'), _use_new_zipfile_serialization=False)

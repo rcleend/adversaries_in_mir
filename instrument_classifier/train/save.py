38b9953a702e919381d086720604a700c37b7030
Sander <s.b.koomen@student.utwente.nl>
2022-01-13 13:55:04 +0100

    fix os library

--- instrument_classifier/train/save.py
+++ instrument_classifier/train/save.py
@@ -1,7 +1,7 @@
-import torch
 import os
+import torch
 from instrument_classifier.utils.paths import model_path
 
 
 def save_model(nn, model_name):
-    torch.save(nn.state_dict(), os.path.join(model_path, "/self-trained", model_name), _use_new_zipfile_serialization=False)
+    torch.save(nn.state_dict(), os.path.join(model_path, model_name), _use_new_zipfile_serialization=False)

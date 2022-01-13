ddab96d91bd6799f6a6bfdf4f8459cdb2964285b
Sander <s.b.koomen@student.utwente.nl>
2022-01-13 12:22:49 +0100

    updated method for saving the model

--- instrument_classifier/train/save.py
+++ instrument_classifier/train/save.py
@@ -1,5 +1,8 @@
 import torch
 from instrument_classifier.utils.paths import model_path
+from instrument_classifier.train.train import net
 
-def save_model(net, model_name):
-    torch.save(net.state_dict(), os.path.join(model_path, model_name), _use_new_zipfile_serialization=False)
+def save_model(nn, model_name):
+    torch.save(nn.state_dict(), os.path.join(model_path, model_name), _use_new_zipfile_serialization=False)
+
+save_model(net, "save_test")

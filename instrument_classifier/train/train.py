bba5cd4930fda49b84dd947bbffb9ebe1546e9d2
Rcleend <rcleende@gmail.com>
2022-01-17 10:24:44 +0100

    Added drop_last = true

--- instrument_classifier/train/train.py
+++ instrument_classifier/train/train.py
@@ -27,6 +27,7 @@ def get_data_loader(valid=True, batch_size=16):
                       valid=valid
                       )
 
+    print('added drop last')
     return DataLoader(ads, batch_size, shuffle=True, drop_last=True)
 
 

72024a5e7f2155c5cc6fea2244a3d753fa7defea
Maartje Huveneers <m.b.huveneers@student.utwente.nl>
2022-01-17 10:46:59 +0100

    Print epoch statement

--- instrument_classifier/train/train.py
+++ instrument_classifier/train/train.py
@@ -44,6 +44,7 @@ def train(net, optimizer, scheduler, criterion, data_loader, n_epoch, device, ba
           print(('lr = {}'.format(scheduler.get_lr())))
 
         net.train(True)  # Put the network in train mode
+        print("epoch ", epoch, " started")
         for i, (x_batch, y_batch) in enumerate(data_loader):
             print(len(x_batch))
             if len(x_batch)!=batch_size:

ff64495ff462967ae7f5acf1ffbd9dcbbd17cdce
Maartje Huveneers <m.b.huveneers@student.utwente.nl>
2022-01-17 10:26:57 +0100

    Batch size 16

--- instrument_classifier/train/train.py
+++ instrument_classifier/train/train.py
@@ -92,7 +92,7 @@ def eval(net, data_loader, device):
 
 # Parameters ---------------------------------------------------------------
 n_epoch = 200
-batch_size = 1
+batch_size = 16
 is_training = True
 model_name='save_test'
 

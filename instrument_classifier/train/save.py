05029c4ed897cf325f0ed77bb2557293903bafa3
Maartje Huveneers <m.b.huveneers@student.utwente.nl>
2022-01-18 12:44:37 +0100

    comment out old code

--- instrument_classifier/train/save.py
+++ instrument_classifier/train/save.py
@@ -1,11 +1,15 @@
-import os
-import torch
-from instrument_classifier.utils.paths import model_path
 
+# THIS IS OUR OWN -OUTDATED- SAVING CODE. SEE TRAINING FOR THE CODE OF THE PAPER!
 
-def save_model(nn, EPOCH):
-    model_name = "model_ep" + str(EPOCH)
-    torch.save({
-        'epoch': EPOCH,
-        'model_state_dict': nn.state_dict(),},
-        os.path.join(model_path, 'self_trained_torch/', model_name + '.tar'))
+#
+# import os
+# import torch
+# from instrument_classifier.utils.paths import model_path
+#
+#
+# def save_model(nn, EPOCH):
+#     model_name = "model_ep" + str(EPOCH)
+#     torch.save({
+#         'epoch': EPOCH,
+#         'model_state_dict': nn.state_dict(),},
+#         os.path.join(model_path, 'self_trained_torch/', model_name + '.tar'))

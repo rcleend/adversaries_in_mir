e8a4e081b4c35ee6f460b4da3e1180e1d5e7f574
Rcleend <rcleende@gmail.com>
2022-01-21 23:16:46 +0100

    .

--- instrument_classifier/defence/defence.py
+++ instrument_classifier/defence/defence.py
@@ -12,10 +12,10 @@ def _avg_def_pred(predictions):
         avg_pred_class + pred
 
 def update_pred_dict(predictions, key, values):
-    if not predictions[key]:
-        predictions.update(key, values)
-    else:
+    if key in predictions:
         predictions[key]['pred'] += values
+    else:
+        predictions[key] =  values
     print(predictions)
     return predictions
 
@@ -28,13 +28,13 @@ def _eval_def_nets(def_nets, data_loader, device):
         for j, (x, y, sample_name) in enumerate(data_loader):
             x, y = x.to(device), y.to(device)  # Move the data to the device that is used
             y_pred = net(x)
-            print(sample_name[0].rsplit('.', 1)[0] + '.wav')
+            print(sample_name[0])
             
             # y_pred_prob = torch.max(nn.functional.softmax(y_pred, dim=1))
             # y_pred_class = torch.argmax(y_pred, dim=1)
 
             print(f'net: {i + 1}, sample {j}/{dataset_size}')
-            predictions = update_pred_dict(predictions, key=sample_name[0].rsplit('.', 1)[0] + '.wav', values={'label': y, 'pred': y_pred})
+            predictions = update_pred_dict(predictions, key=sample_name[0] + '.wav', values={'label': y, 'pred': y_pred})
         # TODO: analyse and store prediction probabilities
 
 # Load Cuda device if available

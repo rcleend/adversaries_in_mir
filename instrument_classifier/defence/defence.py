115f38a8c0be902226e4f49eae0f94826fa9b4d9
Rcleend <rcleende@gmail.com>
2022-01-21 12:44:23 +0100

    Added -1 epoch to get_network method

--- instrument_classifier/defence/defence.py
+++ instrument_classifier/defence/defence.py
@@ -40,7 +40,7 @@ n_defence_nets = 3 #TODO replace with automatic directory detection or parameter
 nets = []
 for i in range(n_defence_nets):
     model_name = f'defence_{i}'
-    nets.append(get_network(model_name=model_name)) # add defence network to nets array
+    nets.append(get_network(model_name=model_name, epoch=-1)) # add defence network to nets array
 
 # Iterate through all the defence networks and average their baseline probabilities
 eval_def_nets(nets, orig_loader, device)

da6e4580db5c15002c255f79c14d5235314af49d
Rcleend <rcleende@gmail.com>
2022-01-21 23:37:24 +0100

    .

--- instrument_classifier/defence/defence.py
+++ instrument_classifier/defence/defence.py
@@ -4,16 +4,20 @@ import torch
 import torch.nn as nn
 from instrument_classifier.evaluation.evaluation_utils import get_data, get_network
 
-def _avg_def_pred(predictions):
-    avg_pred_prob = 0
-    avg_pred_class = 0
+def _avg_pred_dict(predictions): 
+    avg_pred = {}
+    for key, value in predictions.items():
+        y = value['y']
+        y_pred = value['y_pred'] / n_defence_nets
+        y_pred_prob = torch.max(nn.functional.softmax(y_pred, dim=1))
+        y_pred_class = torch.argmax(y_pred, dim=1)
 
-    for pred in predictions:
-        avg_pred_class + pred
+        avg_pred[key] = {'y': y, 'y_pred_class': y_pred_class, 'y_pred_prob': y_pred_prob} 
+    print(avg_pred)
 
-def update_pred_dict(predictions, key, values):
+def _update_pred_dict(predictions, key, values):
     if key in predictions:
-        predictions[key]['pred'] += values['pred']
+        predictions[key]['y_pred'] += values['y_pred']
     else:
         predictions[key] =  values
     return predictions
@@ -31,11 +35,9 @@ def _eval_def_nets(def_nets, data_loader, device):
             # y_pred_prob = torch.max(nn.functional.softmax(y_pred, dim=1))
             # y_pred_class = torch.argmax(y_pred, dim=1)
 
-            print(f'net: {i + 1}, sample {j}/{dataset_size}')
-            predictions = update_pred_dict(predictions, key=sample_name[0], values={'label': y, 'pred': y_pred})
-            if j == 0:
-                print(predictions)
-        # TODO: analyse and store prediction probabilities
+            predictions = _update_pred_dict(predictions, key=sample_name[0], values={'y': y, 'y_pred': y_pred})
+    
+    _avg_pred_dict(predictions)
 
 # Load Cuda device if available
 if torch.cuda.is_available():

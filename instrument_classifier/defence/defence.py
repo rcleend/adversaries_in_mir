cfaed364d9267ce725b17f62fa0f358552332d05
Rcleend <rcleende@gmail.com>
2022-01-22 16:48:23 +0100

    Fully implemented defence

--- instrument_classifier/defence/defence.py
+++ instrument_classifier/defence/defence.py
@@ -18,7 +18,7 @@ def _get_pred(nets, data_loader, pred_name, device):
     all_pred_df = pd.DataFrame() # Create dataframe to store all the predictions
 
     dataset_size = len(data_loader.dataset) # Get dataset_size and use it as an input for the progress bar
-    with tqdm(total=dataset_size, desc=f'Computing {pred_name}', bar_format="{l_bar}{bar} [ time left: {remaining} ]") as pbar:
+    with tqdm(total=dataset_size, desc=f'Computing predictions {pred_name}', bar_format="{l_bar}{bar} [ time left: {remaining} ]") as pbar:
 
         # Iterate through all the samples
         for i, (x, y, sample_name) in enumerate(data_loader):
@@ -32,12 +32,14 @@ def _get_pred(nets, data_loader, pred_name, device):
                 y_pred_sum += net(x) # Update sum of predicted y's
             
             y_avg = y_pred_sum /len(nets) # Calculate average predicted y based on sum of predicted y's
-            y_avg_class = torch.argmax(y_avg, dim=1) # Get the final predicted class based on the average predicion of all the networkds
+            y_avg_pred = torch.argmax(y_avg, dim=1) # Get the predicted class based on the average predicion of all the networkds
             y_avg_prob = torch.max(nn.functional.softmax(y_avg, dim=1)) # Get the probability of the average predicted class
+            y_act_prob = nn.functional.softmax(y_avg[y.item()],dim=1) # Get the predicted probability of the actuall class
+
 
             # Create dataframe containing new prediction
-            new_pred_df = pd.DataFrame(data=[[sample_name[0], y.item(), y_avg_class.item(), y_avg_prob.item()]]) 
-            new_pred_df.columns =['Sample Name', 'Label', f'Pred {pred_name}', f'Prob pred {pred_name}']
+            new_pred_df = pd.DataFrame(data=[[sample_name[0], y.item(), y_avg_pred.item(), y_act_prob.item(), y_avg_prob.item()]]) 
+            new_pred_df.columns =['Sample Name', 'Label', f'Pred {pred_name}', f'Prob act {pred_name}', f'Prob pred {pred_name}']
 
             all_pred_df = all_pred_df.append(new_pred_df) # Add new prediction to dataframe containing all previous predictions
     
@@ -77,14 +79,14 @@ for i in range(n_defence_nets):
     nets.append(get_network(model_name=model_name, epoch=-1).to(device)) # add defence network to nets array
 
 # Get predictions of all the data using the original network
-pred_orig_net = _get_pred(nets=[orig_net], data_loader=orig_loader, pred_name='orig net using orig data', device=device)
-pred_fgsm_net = _get_pred(nets=[orig_net], data_loader=fgsm_loader, pred_name='orig net using FGSM data', device=device)
-pred_pgdn_net = _get_pred(nets=[orig_net], data_loader=pgdn_loader, pred_name='orig net using PGDN data', device=device)
+pred_orig_net = _get_pred(nets=[orig_net], data_loader=orig_loader, pred_name='orig net / orig data', device=device)
+pred_fgsm_net = _get_pred(nets=[orig_net], data_loader=fgsm_loader, pred_name='orig net / FGSM data', device=device)
+pred_pgdn_net = _get_pred(nets=[orig_net], data_loader=pgdn_loader, pred_name='orig net / PGDN data', device=device)
 
 # Get predictions of all the data using the defence networks
-pred_orig_def = _get_pred(nets=nets, data_loader=orig_loader, pred_name='def using orig data', device=device)
-pred_fgsm_def = _get_pred(nets=nets, data_loader=fgsm_loader, pred_name='def using FGSM data', device=device)
-pred_pgdn_def = _get_pred(nets=nets, data_loader=pgdn_loader, pred_name='def using PGDN data', device=device)
+pred_orig_def = _get_pred(nets=nets, data_loader=orig_loader, pred_name='defence / orig data', device=device)
+pred_fgsm_def = _get_pred(nets=nets, data_loader=fgsm_loader, pred_name='defence / FGSM data', device=device)
+pred_pgdn_def = _get_pred(nets=nets, data_loader=pgdn_loader, pred_name='defence / PGDN data', device=device)
 
 # Concat all the pred df together into a total pred df
 total_pred_df = pred_orig_net

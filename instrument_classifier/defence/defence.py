a64653ba3e4b100cb9b04b644db0547abd889032
Rcleend <rcleende@gmail.com>
2022-01-22 08:36:06 +0100

    .

--- instrument_classifier/defence/defence.py
+++ instrument_classifier/defence/defence.py
@@ -12,14 +12,14 @@ def _add_pred_to_csv(data_name, sample_name, y, y_avg):
     y_avg_class = torch.argmax(y_avg, dim=1)
     y_avg_prob = torch.max(nn.functional.softmax(y_avg, dim=1))
 
-    with open(os.path.join(misc_path, f'defences/defence_{data_name}.csv'), 'w', encoding='UTF8') as f:
+    with open(os.path.join(misc_path, f'defences/defence_{data_name}.csv'), 'a', encoding='UTF8') as f:
         writer = csv.writer(f)
 
         # write the header
         # writer.writerow()
 
         # write the data
-        writer.writerow([sample_name, y.item(), y_avg_class.item(), y_avg_prob])
+        writer.writerow([sample_name, y.item(), y_avg_class.item(), y_avg_prob.item()])
 
     # print(sample_name, y.item(), y_avg_class.item(), y_avg_prob.item())
 

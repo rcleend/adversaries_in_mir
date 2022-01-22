bd8a755cb9e02455c009006f83ebd923a878f441
Rcleend <rcleende@gmail.com>
2022-01-22 16:25:15 +0100

    .

--- instrument_classifier/defence/defence.py
+++ instrument_classifier/defence/defence.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 def _add_preds_to_csv(df):
-    csv_path = os.path.join(misc_path, f'defences/defence_{defence_name}.csv')
+    csv_path = os.path.join(misc_path, f'defences/defence_{defence_name}')
     df.to_csv(csv_path, index=False) # Save CSV
 
 
@@ -92,7 +92,7 @@ pred_orig_def = _get_pred(nets=nets, data_loader=orig_loader, pred_name='orig de
 total_pred_df = pred_orig_net
 # total_pred_df = pd.concat([total_pred_df, pred_fgsm_net.reindex(total_pred_df.index)], axis=1)
 # total_pred_df = pd.concat([total_pred_df, pred_pgdn_net.reindex(total_pred_df.index)], axis=1)
-total_pred_df = total_pred_df.merge(pred_orig_def, how='inner', on='Sample Name')
+total_pred_df = total_pred_df.merge(pred_orig_def, how='inner', on=['Sample Name', 'Label'])
 # total_pred_df = pd.concat([total_pred_df, pred_fgsm_def.reindex(total_pred_df.index)], axis=1)
 # total_pred_df = pd.concat([total_pred_df, pred_pgdn_def.reindex(total_pred_df.index)], axis=1)
 
